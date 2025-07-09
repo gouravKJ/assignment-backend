80325509684d7ed9bc5267ffa0e0b0897c397c89
Gourav Kumar Jaiswal <gouravjaiswal2006@gmail.com>
2025-07-10 02:10:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,8 @@ app.get("/email/:emailid",(req,res)=>{
 });
 
 
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT,'0.0.0.0',()=>{
     console.log(`server is running at ${PORT}`);
 });
